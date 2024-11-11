d2053fdf8114b751cd01d8d4a5ff080147acb781
DaviddairoT <dairotemitopedavid@gmail.com>
2024-11-11 15:09:33 +0100

    hy

--- controller/task.js
+++ controller/task.js
@@ -6,6 +6,18 @@ const createTask = (req, res) => {
     res.send('this is the post side')
 }
 
+const getTask = (req, res) =>{
+    res.send('get single content')
+}
+
+const updateTask = (req, res) => {
+    res.send('update your task')
+}
+
+const deleteTask = (req, res) =>  {
+    res.send('this is the delete')
+}
+
 module.exports = {
-    getAllTasks, createTask,
+    getAllTasks, createTask, getTask, updateTask, deleteTask,
 }
--- routes/task.js
+++ routes/task.js
@@ -1,14 +1,14 @@
 const express = require('express');
 const router = express.Router();
 
-const { getAllTasks } = require('../controller/task');
+const { getAllTasks, getTask, updateTask, deleteTask, } = require('../controller/task');
 const {createTask} =  require('../controller/task.js');
 
 
 // Define the route correctly
 router.get('/', getAllTasks);
 router.post('/create', createTask);
-
+router.get('/:id', getTask).patch('/:id', updateTask).delete('/:id', deleteTask);
 
 module.exports = router;
 
